c0333569dc5d639aa14ece2f22aced31d45c356d
SANDEEP YADAV <36203125+SANDEEP0181@users.noreply.github.com>
2026-09-16 19:09:58 +0530

    Configure SYSFI testnet network

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,4 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.24",
+
+  networks: {
+    sysfi: {
+      url: "https://rpc-endpoint.sysfi.network",
+      chainId: 76081
+    }
+  }
 };
