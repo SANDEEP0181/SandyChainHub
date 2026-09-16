24a44a74ba492669a338cdd1ce58a22b3cb8de7a
SANDEEP YADAV <36203125+SANDEEP0181@users.noreply.github.com>
2026-09-16 19:22:14 +0530

    Align local Hardhat tests with SYSFI testnet chain ID

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,9 @@ module.exports = {
   solidity: "0.8.24",
 
   networks: {
+    hardhat: {
+      chainId: 76081
+    },
     sysfi: {
       url: "https://rpc-endpoint.sysfi.network",
       chainId: 76081
