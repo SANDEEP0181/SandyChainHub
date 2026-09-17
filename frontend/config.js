a37eebe6140fb302b947bec67919753c8eaecd57
SANDEEP YADAV <YSANDEEP336@GMAIL.COM>
2026-09-17 19:34:17 +0530

    Configure deployed SysFi contract address

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 // SYSFI testnet configuration.
 // Never put a private key or seed phrase in this file.
 window.APP_CONFIG = {
-  contractAddress: "",
+  contractAddress:contractAddress:"0x07EAa7d720E9BAe0F0083ac4c8640356F491eb87",
   chainId: "76081",
   explorerBaseUrl: "https://explorer.sysfi.network"
 };
