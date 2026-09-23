8ebf5202c0f1d1cd88be027bc69b04c2241d1af2
SANDEEP YADAV <36203125+SANDEEP0181@users.noreply.github.com>
2026-09-23 18:26:32 +0530

    Security: shorten Telegram auth freshness window

--- api/_lib/goalkeeper.js
+++ api/_lib/goalkeeper.js
@@ -1,7 +1,7 @@
 import crypto from "node:crypto";
 
 export const ALLOWED_ORIGIN = "https://sandeep0181.github.io";
-export const MAX_AUTH_AGE_SECONDS = 24 * 60 * 60;
+export const MAX_AUTH_AGE_SECONDS = 60 * 60;
 
 export function cors(res, methods = "POST, OPTIONS") {
   res.setHeader("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
